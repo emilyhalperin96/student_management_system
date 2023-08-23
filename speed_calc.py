20ade7e6db017a53454af850a9be8977c0bde324
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP-2.lan>
2023-08-23 16:36:49 -0400

    convert inputs to floats

--- speed_calc.py
+++ speed_calc.py
@@ -15,13 +15,8 @@ class SpeedCalculator(QWidget):
         time_label = QLabel('Time (hours):')
         self.time_label_edit = QLineEdit()
 
-        metric_label = QComboBox()
-        metric_label.addItems(['km', 'other'])
-
-        if metric_label.currentText() == 'km':
-            'do something'
-        if metric_label.currentText() == 'other':
-            'do something else'
+        self.metric_label = QComboBox()
+        self.metric_label.addItems(['Metric (km)', 'Imperial (miles)'])
 
         calculate_button = QPushButton('Calculate Speed')
         calculate_button.clicked.connect(self.calculate_speed)
@@ -31,20 +26,35 @@ class SpeedCalculator(QWidget):
 
         grid.addWidget(distance_label, 0, 0)
         grid.addWidget(self.distance_label_edit, 0, 1)
+        grid.addWidget(self.metric_label, 0, 2)
         grid.addWidget(time_label, 1, 0)
         grid.addWidget(self.time_label_edit, 1, 1)
         grid.addWidget(calculate_button, 2, 0, 1, 2) #span accross one row and 2 columns 
         grid.addWidget(self.output_label, 3, 0, 1, 2) #span accross one row and 2 columns 
 
-        grid.addWidget(metric_label, 2, 2)
         
         self.setLayout(grid)
 
     
     def calculate_speed(self):
-        speed = self.distance_label_edit.text() // self.time_label_edit.text()
-        self.output_label.setText('f Average Speed: speed')
+        #get distance and time from input 
+        distance = float(self.distance_input.text())
+        time = float(self.time_input.text())
 
+        #speed 
+        speed = distance/time 
+
+        #check combo 
+
+        if self.metric_label.currentText() == 'Metric (km)':
+            speed = round(speed, 2)
+            unit = 'km/h'
+        if self.metric_label.currentText() == 'Imperial (miles)':
+            speed = round(speed * 0.621361, 2)
+            unit = 'mph'
+        
+        #display result
+        
 
 app = QApplication(sys.argv)
 speed_calculator = SpeedCalculator()
